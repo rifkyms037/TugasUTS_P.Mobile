fe3483057b627f030e5f7ff5975002e47aad5a01
rifkyms23 <Rifky MS>
2023-04-14 21:46:49 +0700

    Update news berita

---
+++ lib/data/news_data.dart
@@ -0,0 +1,227 @@
+import '../models/news.dart';
+
+List<News> newsList = [
+  News(
+    id: 1,
+    title:
+        "Warga yang Lawan Pecalang saat Nyepi Dimediasi Tapi Belum Capai Kesepakatan",
+    image:
+        "https://media.suara.com/pictures/653x366/2022/02/24/77463-ilustrasi-mengenal-4-prosesi-dan-makna-hari-raya-nyepi-bagi-umat-hindu-freepik.webp",
+    content: '''
+Pada saat perayaan Hari Nyepi di Bali yang jatuh pada Rabu (22/3/2023) lalu, umat Hindu dan seluruh warga Bali diharap untuk tidak bepergian, terlebih dengan kendaraan bermotor.
+
+Hal ini merupakan aturan yang sudah berjalan sejak lama dan selalu ditaati warga di Bali.
+
+Namun, sebuah video yang viral di media sosial menunjukkan seseorang melawan pecalang dan berusaha membuka palang yang menutup jalan di Desa Sumberklampok, Kecamatan Gerokgak, Kabupaten Buleleng.
+
+Dalam video tersebut juga nampak beberapa sepeda motor yang terlihat mengantre di belakang palang untuk memasuki kawasan pantai di TNBB Buleleng tersebut.
+
+Dalam keterangan dari Kepala Seksi Humas Polres Buleleng AKP Gede Sumarjaya, Polsek Gerokgak akhirnya menangkap dua orang berinisial AS dan MR terkait kasus tersebut.
+
+“Hasil koordinasi yang dilaksanakan kemudian sekira pukul 23.00 wita yang bersangkutan dibawa ke Polsek Gerokgak,” ujar Sumarjaya saat dihubungi pada Jumat (24/3/2023).
+        ''',
+  ),
+  News(
+    id: 2,
+    title:
+        "Gerokgak Tegang, Provokator Perayaan Nyepi di Bali Akhirnya Diamankan Polisi",
+    image:
+        "https://media.suara.com/suara-partners/denpasar/thumbs/653x367/2023/03/23/1-gerogak.jpg",
+    content: '''
+Video yang menggambarkan kasus buka paksa Portal ke Segara Rupek yang viral oleh seorang warga menggunakan baju loreng membuat suasana di Gerokgak, Kabupaten Buleleng, Provinsi Bali, sempat tegang.
+
+Warga yang tidak terima dengan ulah pria yang dituding provokator dan mengganggu jalannya Nyepi itu pun mulai berkumpul di sepanjang jalur utama Jembrana-Negara.
+
+Untuk diketahui, saat Nyepi akses ke Pantai Segara Rupek memang ditutup untuk umum. Tapi, beberapa warga pendatang malah memaksa masuk dan membuka portal dan dinilai tidak menghormati Hari Raya Nyepi.
+
+Mereka juga dinilai sudah melecehkan pecalang atau pengamanan adat yang mengingatkan bahwa saat ini Nyepi.
+
+"Tapi, pria berbaju loreng itu ngotot bahkan berkoar dirinya tidak takut. Meski Kapolda Bali datang ke lokasi sembari mempersilahkan warga yang datang dengan sepeda motor untuk masuk ke lokasi.
+
+Viralnya video itu di media sosial direspons cepat jajaran Polsek Gerokgak. Saat Nyepi atau Rabu 22 Maret 2023 malam, petugas mengamankan dua pria yang dituding sebagai provokator termasuk si pria berbaju loreng.
+
+''',
+  ),
+  News(
+    id: 3,
+    title: "Rayakan Ulang Tahun ke-9, Suara.com Luncurkan Nexus Creator Hub",
+    image:
+        "https://media.suara.com/pictures/970x544/2023/03/13/83672-ulang-tahun-9-suaracom-suwarjono.jpg",
+    content: '''
+Direktur Utama PT Arkadia Digital Media, Suwarjono memberikan sambutan saat perayaan Ulang Tahun ke-9 Suara.com di Kuningan, Jakarta, Senin (13/3/2023). Bersamaan dengan perayaan ulang tahun ke-9 portal berita Suara.com, Arkadia Digital Media, selaku perusahaan induk meluncurkan platform sekaligus program baru bernama Nexus Creator Hub.
+
+Nexus Creator Hub menjadi wadah yang berisi ratusan kreator konten dari berbagai platform media sosial telah bergabung, baik untuk mengikuti program inkubasi maupun partnership.
+
+Dalam sambutannya, Suwarjono menjelaskan peluncuran Nexus Creator Hub ini beranjak dari perkembangan terkini di dunia digital, di mana keberadaan dan peran para content creator, seperti influencer atau key opinion leaders (KOL), semakin dibutuhkan.
+
+“Selain produk jurnalisme, stakeholder media juga membutuhkan keberadaan para content creator, influencer, key opinion leaders. Hal ini membuat permintaan dari brand, perusahaan, maupun lembaga, terhadap mereka pun semakin tinggi untuk dilibatkan dalam campaign-campaign," ungkap Suwarjono.
+
+''',
+  ),
+  News(
+    id: 4,
+    title: "Jefri Nichol Damprat Penulis Media Online: Orang Gak Punya Otak",
+    image:
+        "https://media.suara.com/suara-partners/selebtek/thumbs/653x367/2022/09/02/1-jefri-nichol.jpg",
+    content: '''
+Jefri Nichol ngamuk di Twitter dan menyemprot penulis di salah satu portal media online. Ia kesal gara-gara sebuah artikel terkait dirinya.
+
+Melalui cuitannya, Jefri Nichol menyoroti artikel yang berjudul "Ring Tinju Jefri Nichol, Simbol Kemunduran Peradaban."  
+
+Jefri Nichol lantas me-retweet cuitan akun portal tersebut. Tak hanya itu, ia meluapkan kekesalannya dengan kata-kata kasar dan menandai langsung akun si penulis.
+
+"Kumparan, lo coba nyari jurnalis yang enggak b*go-b*go amat dah, nyari info yang ada di medsos aja salah," cuit Jefri Nichol.
+
+Tidak sampai di situ, Jefri Nichol menyentil kata-kata yang tertulis dalam judul artikel. Ia tak terima aksi adu jotos di dalam ring tinju dianggap kemunduran peradaban.
+
+Ia menyebut si penulis tidak becus dalam bekerja lantaran tidak melakukan riset dengan benar. 
+"Ngomong soal kemunduran peradaban tapi riset aja kaga becus @prabu_***," cuitnya.
+
+Setelah ditelusuri, akun penulis artikel tersebut seolah menjawab kekesalan Jefri Nichol melalui deskripsi dalam bio-nya.
+
+"Kalau tulisan saya menyinggung Anda, saya tidak peduli," tulis @prabu_*** sambil menyematkan emoji jari tengah.
+''',
+  ),
+  News(
+      id: 5,
+      title: "Diskominfo-SP Luncurkan Portal Satu Data Sulawesi Selatan",
+      image:
+          "https://media.suara.com/pictures/653x366/2022/11/18/78435-satu-data.webp",
+      content: '''
+Dinas Komunikasi, Informatika, Statistik dan Persandian (Diskominfo-SP) Provinsi Sulawesi Selatan meluncurkan Portal Satu Data Sulawesi Selatan Tahun 2022 di Gammara Hotel Makassar, Jumat (18/11/2022).
+
+Peluncuran portal tersebut merupakan rangkaian dari kegiatan peningkatan mutu statistik daerah dan sekaligus Pengukuhan Forum Satu Data Sulsel.
+
+Kegiatan tersebut dihadiri oleh perwakilan BPS Sulsel, Pejabat Administrator dan Pengawas Lingkup Diskominfo-SP Sulsel, perwakilan Dinas Kominfo, Dinas Statistik, dan Bappelitbangda Kabupaten/Kota se-Sulsel, serta pengelola data sektoral Perangkat Daerah Lingkup Pemprov Sulsel.
+
+Dalam sambutannya mewakili Gubernur Sulsel, Asisten III Bidang Administrasi Tautoto Tana Ranggina mengapresiasi kegiatan peluncuran Portal Satu Data, Pengukuhan Forum Satu Data dan MOU untuk mendukung Satu Data Sulsel tersebut.
+
+"Ini sebagai bagian dari penguatan koordinasi serta komitmen bersama. Dalam mewujudkan satu data Indonesia, khususnya di tingkat Provinsi Sulawesi Selatan, serta mendukung proses pengambilan keputusan berbasis data," kata Tautoto.
+
+Lebih lanjut, ia mengatakan bahwa data mempunyai peranan penting dalam penyusunan, perencanaan, pelaksanaan dan pengendalian pembangunan suatu daerah.
+'''),
+  News(
+    id: 6,
+    title: "Usai PHK, Meta Hentikan Produksi Portal dan Jam Tangan Pintar",
+    image:
+        "https://media.suara.com/pictures/653x366/2021/07/04/85878-ilustrasi-smartwatch.webp",
+    content: '''
+Badai PHK 11.000 karyawan Meta rupanya berdampak pada produk perusahaan. Perangkat seperti Portal dan smartwatch yang belum dirilis pun gagal diproduksi.
+
+Dilaporkan The Verge, Minggu (13/11/2022), Meta diam-diam menghentikan proyek produk layar pintar Portal hingga smartwatch yang saat ini masih dalam tahap pengembangan.
+
+Jam tangan pintar yang diberi nama kode 'Milan' ini awalnya bakal dirilis pada musim semi Amerika Serikat tahun 2023 dengan harga 349 Dolar AS atau Rp 5,4 juta.
+
+Smartwatch buatan Meta ini akan membawa fitur seperti dua kamera built-in yang bisa dipakai untuk video call. Pengembangan produk ini pun sudah dilakukan sejak Juni 2021.
+
+Selain smartwatch, Meta juga menghentikan produk Portal yang sudah dirilis pada Juni 2022 lalu.
+
+Sebelumnya, produk layar pintar ini dijual untuk konsumen, yang kemudian direncanakan rilis hanya untuk pelanggan bisnis.
+
+Kini produk Portal dan smartwatch diam-diam dihentikan Meta, imbas dari PHK 11.000 karyawan yang juga mencakup divisi teknologi. Meta pun tidak menanggapi rumor penghentian produk ini.
+
+PHK Meta sendiri berdampak pada 11.000 karyawan, yang mana 46 persen dari total pekerja adalah mereka yang berada di divisi teknologi.
+
+Tidak jelas juga berapa banyak dari mereka yang bertugas di divisi hardware atau perangkat keras yang terdampak efek PHK.
+''',
+  ),
+  News(
+    id: 7,
+    title:
+        "Sebelum Lakukan Blokir pada PSE, Kominfo Akan Ajukan Teguran Tertulis Pada Platform Online",
+    image:
+        "https://akcdn.detik.net.id/visual/2020/01/22/ecdf377b-098f-4b9a-a60b-b2dfa089d153_169.jpeg?w=650&q=90",
+    content: '''
+Kementerian Komunikasi dan Informatika menetapkan Rabu, 20 Juli 2022, sebagai batas akhir pendaftaran penyelenggara sistem elektronik (PSE) lingkup privat di mana registrasi tersebut bisa dilakukan di situs resmi Online Single Submission (OSS) oss.go.id kemudian situs layanan.kominfo.go.id.
+
+Direktorat Jenderal Aplikasi Informatika Kominfo menyebutkan bahwa tata kelola PSE lingkup privat diatur melalui Peraturan Pemerintah Nomor 71 Tahun 2019 tentang Penyelenggaraan Sistem dan Transaksi Elektronik dan Peraturan Menteri Kominfo Nomor 10 Tahun 2021 atas Perubahan Peraturan Menteri Kominfo Nomor 5 Tahun 2020 tentang Penyelenggara Sistem Elektronik Lingkup Privat.
+
+Berdasarkan kedua aturan itu, ada enam kategori PSE yang wajib mendaftar. Pertama, PSE privat wajib mendaftar jika menyediakan, mengelola dan atau mengoperasikan penawaran dan atau perdagangan barang dan atau jasa.
+
+Kedua, PSE yang menyediakan atau mengelola dan atau mengoperasikan layanan transaksi keuangan. Ketiga, PSE yang melakukan pengiriman materi atau muatan digital berbayar melalui jaringan data, baik dengan cara unduh melalui portal atau situs pengiriman, surat elektronik, atau melalui aplikasi.
+
+Keempat, PSE wajib daftar ke Kominfo jika menyediakan, mengelola dan atau mengoperasikan layanan komunikasi, meliputi, namun, tidak terbatas pada pesan singkat, panggilan suara, panggilan video, surat elektronik dan percakapan dalam jaringan, dalam bentuk platform digital layanan jejaring dan media sosial.
+
+Kelima, PSE menyediakan layanan mesin pencari, penyediaan informasi elektronik berbentuk tulisan, gambar, suara, video, animasi, musik, film dan permainan, atau kombinasi dari sebagian dan atau seluruhnya.
+
+Keenam, PSE yang memproses data pribadi untuk kegiatan operasional, melayani masyarakat terkait dengan aktivitas transaksi elektronik.
+
+Sebelum mengikuti pendaftaran, PSE harus terlebih dulu menyelesaikan pengurusan Nomor Induk Berusaha (NIB) dan mengajukan izin operasional atau komersial ke OSS yang dikelola oleh Badan Koordinasi Penanaman Modal.
+
+Berdasarkan dokumen resmi di situs Kominfo, PSE yang sudah menyelesaikan tahap di atas akan memperoleh nama pengguna (username), kata sandi dan tautan. Setelah itu, buka situs layanan.kominfo.go.id dan masuk dengan kata username dan kata sandi.
+''',
+  ),
+];
+
+List<News> hotNewsList = [
+  News(
+    id: 4,
+    title: "Jefri Nichol Damprat Penulis Media Online: Orang Gak Punya Otak",
+    image:
+        "https://media.suara.com/suara-partners/selebtek/thumbs/653x367/2022/09/02/1-jefri-nichol.jpg",
+    content: '''
+Jefri Nichol ngamuk di Twitter dan menyemprot penulis di salah satu portal media online. Ia kesal gara-gara sebuah artikel terkait dirinya.
+
+Melalui cuitannya, Jefri Nichol menyoroti artikel yang berjudul "Ring Tinju Jefri Nichol, Simbol Kemunduran Peradaban."  
+
+Jefri Nichol lantas me-retweet cuitan akun portal tersebut. Tak hanya itu, ia meluapkan kekesalannya dengan kata-kata kasar dan menandai langsung akun si penulis.
+
+"Kumparan, lo coba nyari jurnalis yang enggak b*go-b*go amat dah, nyari info yang ada di medsos aja salah," cuit Jefri Nichol.
+
+Tidak sampai di situ, Jefri Nichol menyentil kata-kata yang tertulis dalam judul artikel. Ia tak terima aksi adu jotos di dalam ring tinju dianggap kemunduran peradaban.
+
+Ia menyebut si penulis tidak becus dalam bekerja lantaran tidak melakukan riset dengan benar. 
+"Ngomong soal kemunduran peradaban tapi riset aja kaga becus @prabu_***," cuitnya.
+
+Setelah ditelusuri, akun penulis artikel tersebut seolah menjawab kekesalan Jefri Nichol melalui deskripsi dalam bio-nya.
+
+"Kalau tulisan saya menyinggung Anda, saya tidak peduli," tulis @prabu_*** sambil menyematkan emoji jari tengah.
+''',
+  ),
+  News(
+    id: 7,
+    title:
+        "Sebelum Lakukan Blokir pada PSE, Kominfo Akan Ajukan Teguran Tertulis Pada Platform Online",
+    image:
+        "https://media.suara.com/suara-partners/tantrum/thumbs/653x367/2022/07/20/1-ilustrasi-media-sosial.jpg",
+    content: '''
+Kementerian Komunikasi dan Informatika menetapkan Rabu, 20 Juli 2022, sebagai batas akhir pendaftaran penyelenggara sistem elektronik (PSE) lingkup privat di mana registrasi tersebut bisa dilakukan di situs resmi Online Single Submission (OSS) oss.go.id kemudian situs layanan.kominfo.go.id.
+
+Direktorat Jenderal Aplikasi Informatika Kominfo menyebutkan bahwa tata kelola PSE lingkup privat diatur melalui Peraturan Pemerintah Nomor 71 Tahun 2019 tentang Penyelenggaraan Sistem dan Transaksi Elektronik dan Peraturan Menteri Kominfo Nomor 10 Tahun 2021 atas Perubahan Peraturan Menteri Kominfo Nomor 5 Tahun 2020 tentang Penyelenggara Sistem Elektronik Lingkup Privat.
+
+Berdasarkan kedua aturan itu, ada enam kategori PSE yang wajib mendaftar. Pertama, PSE privat wajib mendaftar jika menyediakan, mengelola dan atau mengoperasikan penawaran dan atau perdagangan barang dan atau jasa.
+
+Kedua, PSE yang menyediakan atau mengelola dan atau mengoperasikan layanan transaksi keuangan. Ketiga, PSE yang melakukan pengiriman materi atau muatan digital berbayar melalui jaringan data, baik dengan cara unduh melalui portal atau situs pengiriman, surat elektronik, atau melalui aplikasi.
+
+Keempat, PSE wajib daftar ke Kominfo jika menyediakan, mengelola dan atau mengoperasikan layanan komunikasi, meliputi, namun, tidak terbatas pada pesan singkat, panggilan suara, panggilan video, surat elektronik dan percakapan dalam jaringan, dalam bentuk platform digital layanan jejaring dan media sosial.
+
+Kelima, PSE menyediakan layanan mesin pencari, penyediaan informasi elektronik berbentuk tulisan, gambar, suara, video, animasi, musik, film dan permainan, atau kombinasi dari sebagian dan atau seluruhnya.
+
+Keenam, PSE yang memproses data pribadi untuk kegiatan operasional, melayani masyarakat terkait dengan aktivitas transaksi elektronik.
+
+Sebelum mengikuti pendaftaran, PSE harus terlebih dulu menyelesaikan pengurusan Nomor Induk Berusaha (NIB) dan mengajukan izin operasional atau komersial ke OSS yang dikelola oleh Badan Koordinasi Penanaman Modal.
+
+Berdasarkan dokumen resmi di situs Kominfo, PSE yang sudah menyelesaikan tahap di atas akan memperoleh nama pengguna (username), kata sandi dan tautan. Setelah itu, buka situs layanan.kominfo.go.id dan masuk dengan kata username dan kata sandi.
+''',
+  ),
+  News(
+    id: 1,
+    title:
+        "Warga yang Lawan Pecalang saat Nyepi Dimediasi Tapi Belum Capai Kesepakatan",
+    image:
+        "https://media.suara.com/pictures/653x366/2022/02/24/77463-ilustrasi-mengenal-4-prosesi-dan-makna-hari-raya-nyepi-bagi-umat-hindu-freepik.webp",
+    content: '''
+Pada saat perayaan Hari Nyepi di Bali yang jatuh pada Rabu (22/3/2023) lalu, umat Hindu dan seluruh warga Bali diharap untuk tidak bepergian, terlebih dengan kendaraan bermotor.
+
+Hal ini merupakan aturan yang sudah berjalan sejak lama dan selalu ditaati warga di Bali.
+
+Namun, sebuah video yang viral di media sosial menunjukkan seseorang melawan pecalang dan berusaha membuka palang yang menutup jalan di Desa Sumberklampok, Kecamatan Gerokgak, Kabupaten Buleleng.
+
+Dalam video tersebut juga nampak beberapa sepeda motor yang terlihat mengantre di belakang palang untuk memasuki kawasan pantai di TNBB Buleleng tersebut.
+
+Dalam keterangan dari Kepala Seksi Humas Polres Buleleng AKP Gede Sumarjaya, Polsek Gerokgak akhirnya menangkap dua orang berinisial AS dan MR terkait kasus tersebut.
+
+“Hasil koordinasi yang dilaksanakan kemudian sekira pukul 23.00 wita yang bersangkutan dibawa ke Polsek Gerokgak,” ujar Sumarjaya saat dihubungi pada Jumat (24/3/2023).
+        ''',
+  ),
+];
